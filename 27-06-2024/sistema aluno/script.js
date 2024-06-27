7066c7ba8f3a5eabafface202cc5c5924bbe0ff6
Lucas-Ramos-Coelho <lucas.rc2105@gmail.com>
2024-06-27 11:46:16 -0300

    final do dia

---
+++ 27-06-2024/sistema aluno/script.js
@@ -0,0 +1,16 @@
+const button = document.querySelector("button");
+const inputNota = document.querySelector("#nota");
+
+//criando uma função
+button.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log(inputNota.value);
+    
+
+    //lógica
+    if(inputNota.value >= 6){
+        alert("Você está aprovado")
+    }else{
+        alert("Você está reprovado")
+    }
+})
